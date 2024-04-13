7d981d869ba1d6f26b786768faf2f8571ea5e992
gabriel-2802 <gabriel@Gabriel28>
2024-04-13 15:03:04 +0300

    stage1 conquering

--- Java/Direction.java
+++ Java/Direction.java
@@ -10,4 +10,19 @@ public enum Direction {
         Direction[] values = values();
         return values[new Random().nextInt(values.length)];
     }
+
+    public static Direction invertDirection(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                return SOUTH;
+            case EAST:
+                return WEST;
+            case SOUTH:
+                return NORTH;
+            case WEST:
+                return EAST;
+            default:
+                return STILL;
+        }
+    }
 }
--- Java/StageOneBot.java
+++ Java/StageOneBot.java
@@ -3,18 +3,25 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class StageOneBot {
 
     private static GameMap gameMap;
     private static int myID;
     private static List<Move> moves = new ArrayList<Move>();
-    private static Set<Location> moveHistory = new HashSet<>();
+    private static Set<Location> myLocations = new HashSet<>();
     private static PriorityQueue<Location> heap = new PriorityQueue<>(new HeapComparator());
 
+
+    // debug
+    private static String file = "output.txt";
+
     private static double getScore(Location location) {
-      return 1.0 * location.getSite().production / location.getSite().strength + 1;
+      return (1.0 * location.getSite().production) / (1.0 * location.getSite().strength) + 1;
     }
 
     private static class HeapComparator implements Comparator<Location> {
@@ -38,6 +45,41 @@ public class StageOneBot {
 
     }
 
+    // checks if location is my neighbour
+    private static boolean isNeighbour(Location location) {
+        for (Direction dir : Direction.DIRECTIONS) {
+            Location newLocation = gameMap.getLocation(location, dir);
+            if (newLocation.getSite().owner == myID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private static List<Entry<Location, Direction>> getConquerors(Location location) {
+        List<Entry<Location, Direction>> conquerors= new ArrayList<>();
+
+        for (Direction dir : Direction.DIRECTIONS) {
+            Location newLocation = gameMap.getLocation(location, dir);
+            if (newLocation.getSite().owner == myID && myLocations.contains(newLocation)) {
+                conquerors.add(new AbstractMap.SimpleEntry<>(newLocation, Direction.invertDirection(dir)));
+            }
+        }
+        return conquerors;
+    }
+
+    private static void conquer(Location location) {
+        List<Entry<Location, Direction>> conquerors = getConquerors(location);
+        for (var conqueror : conquerors) {
+            if (conqueror.getKey().getSite().strength >= location.getSite().strength) {
+                moves.add(new Move(conqueror.getKey(), conqueror.getValue()));
+                myLocations.remove(conqueror.getKey());
+            }
+        }
+    }
+
+
     public static void main(String[] args) throws java.io.IOException {
 
         final InitPackage iPackage = Networking.getInit();
@@ -45,12 +87,14 @@ public class StageOneBot {
         gameMap = iPackage.map;
 
         Networking.sendInit("MyJavaBot");
-    
+        
+        FileWriter writer = new FileWriter(file, true);
+        writer.write("New frame\n");
             
 
         while(true) {
             moves.clear();
-            moveHistory.clear();
+            myLocations.clear();
             heap.clear();
 
             Networking.updateFrame(gameMap);
@@ -67,21 +111,31 @@ public class StageOneBot {
              * 
              */
 
+            
+             // traverse the map and add neighbours to heap
             for (int y = 0; y < gameMap.height; y++) {
                 for (int x = 0; x < gameMap.width; x++) {
-                    // build heap
+                   Location location = gameMap.getLocation(x, y);
+                   if (isNeighbour(location) && location.getSite().owner != myID) {
+                       heap.add(location);
+                   }
+
+                   if (location.getSite().owner == myID) {
+                       myLocations.add(location);
+                   }
                 }
             }
 
             while (!heap.isEmpty()) {
-                // if can conquer, conquer and add to array of moves and set
+                Location location = heap.poll();
+                conquer(location);
             }
 
-            for (int y = 0; y < gameMap.height; y++) {
-                for (int x = 0; x < gameMap.width; x++) {
-                    // move to closest border, functie
-                }
+            for (Location location : myLocations) {
+                moves.add(new Move(location, Direction.STILL));
             }
+
+            
 
 
 
