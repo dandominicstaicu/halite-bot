d641623fc9ef123370722e990063591ec3ac9d6c
dandominicstaicu <dando.ds11@gmail.com>
2024-04-13 16:43:50 +0300

    done stage 1

--- Java/StageOneBot.java
+++ Java/StageOneBot.java
@@ -21,7 +21,7 @@ public class StageOneBot {
     private static String file = "output.txt";
 
     private static double getScore(Location location) {
-      return (1.0 * location.getSite().production) / (1.0 * location.getSite().strength) + 1;
+        return (1.0 * location.getSite().production) / (1.0 * location.getSite().strength) + 1;
     }
 
     private static class HeapComparator implements Comparator<Location> {
@@ -58,7 +58,7 @@ public class StageOneBot {
 
 
     private static List<Entry<Location, Direction>> getConquerors(Location location) {
-        List<Entry<Location, Direction>> conquerors= new ArrayList<>();
+        List<Entry<Location, Direction>> conquerors = new ArrayList<>();
 
         for (Direction dir : Direction.DIRECTIONS) {
             Location newLocation = gameMap.getLocation(location, dir);
@@ -98,27 +98,20 @@ public class StageOneBot {
         Location east = gameMap.getLocation(location, Direction.EAST);
         Location west = gameMap.getLocation(location, Direction.WEST);
 
-
         return north.getSite().owner == site.owner
                 && south.getSite().owner == site.owner
                 && west.getSite().owner == site.owner
                 && east.getSite().owner == site.owner;
     }
 
-    private static void findFurthestOuter(Location far, Direction dir, Site site) {
-        int dist = 0;
-
-        while (far.getSite().owner == site.owner) {
-            if (dir.equals(Direction.NORTH) || dir.equals(Direction.SOUTH)) { // NORTH/SOUTH
-                if (++dist < gameMap.height / 2) {
-                    far = gameMap.getLocation(far, dir);
-                }
-            } else { // EAST/WEST
-                if (++dist < gameMap.width / 2) {
-                    far = gameMap.getLocation(far, dir);
-                }
-            }
+    private static Location findFarthestBoundary(Location start, Direction direction, int limit) {
+        int distance = 0;
+        Location current = start;
+        while (current.getSite().owner == start.getSite().owner && distance < limit) {
+            current = gameMap.getLocation(current, direction);
+            distance++;
         }
+        return current;
     }
 
     private static void checkAndUpdateBestMove(Location location, Location loc, Direction dir, BestMoveTracker tracker) {
@@ -136,12 +129,12 @@ public class StageOneBot {
         gameMap = iPackage.map;
 
         Networking.sendInit("MyJavaBot");
-        
+
         FileWriter writer = new FileWriter(file, true);
         writer.write("New frame\n");
-            
 
-        while(true) {
+
+        while (true) {
             moves.clear();
             myLocations.clear();
             heap.clear();
@@ -150,33 +143,32 @@ public class StageOneBot {
 
             /*
              * parcurgem tot, pt vecini calculam scorul -> heap
-             * 
-             * 
+             *
+             *
              * while !heap_empty:
              *  // verificam daca putem sa cucerim direct, daca nu stam
-             * 
+             *
              * parcurgem inca o data, ce nu a fost mutat (int interior) il facem sa mearga spre exteriorul cel mai apropiat daca are un strenght >= 5 * prod
-             * 
-             * 
+             *
+             *
              */
 
-            
-             // traverse the map and add neighbours to heap
+
+            // traverse the map and add neighbours to heap
             for (int y = 0; y < gameMap.height; y++) {
                 for (int x = 0; x < gameMap.width; x++) {
-                   Location location = gameMap.getLocation(x, y);
-                   if (isNeighbour(location) && location.getSite().owner != myID) {
-                       heap.add(location);
-                   }
+                    Location location = gameMap.getLocation(x, y);
+                    if (isNeighbour(location) && location.getSite().owner != myID) {
+                        heap.add(location);
+                    }
 
-                   if (location.getSite().owner == myID) {
-                       myLocations.add(location);
-                   }
+                    if (location.getSite().owner == myID) {
+                        myLocations.add(location);
+                    }
                 }
             }
 
             // dominic do your magic here. remove them from myLocations after assigning them work
-
 
             for (int y = 0; y < gameMap.height; y++) {
                 for (int x = 0; x < gameMap.width; x++) {
@@ -200,35 +192,10 @@ public class StageOneBot {
                             myLocations.remove(location);
 
                         } else {
-                            Location north = gameMap.getLocation(location, Direction.NORTH);
-                            Location south = gameMap.getLocation(location, Direction.SOUTH);
-                            Location east = gameMap.getLocation(location, Direction.EAST);
-                            Location west = gameMap.getLocation(location, Direction.WEST);
-
-                            findFurthestOuter(north, Direction.NORTH, site);
-                            findFurthestOuter(south, Direction.SOUTH, site);
-                            findFurthestOuter(east, Direction.EAST, site);
-                            findFurthestOuter(west, Direction.WEST, site);
-
-
-//
-//                            PriorityQueue<Move> farMoves = new
-//                                    PriorityQueue<Move>(new Comparator<Move>() {
-//                                @Override
-//                                public int compare(Move o1, Move o2) {
-//                                    return (int) gameMap.getDistance(location, o1.loc) -
-//                                            (int) gameMap.getDistance(location, o2.loc);
-//                                }
-//                            });
-//
-//                            farMoves.add(new Move(north, Direction.NORTH));
-//                            farMoves.add(new Move(south, Direction.SOUTH));
-//                            farMoves.add(new Move(east, Direction.EAST));
-//                            farMoves.add(new Move(west, Direction.WEST));
-//
-//                            // Get the best move from the heap.
-//                            moves.add(new Move(location, farMoves.peek().dir));
-
+                            Location north = findFarthestBoundary(location, Direction.NORTH, gameMap.height / 2);
+                            Location south = findFarthestBoundary(location, Direction.SOUTH, gameMap.height / 2);
+                            Location east = findFarthestBoundary(location, Direction.EAST, gameMap.width / 2);
+                            Location west = findFarthestBoundary(location, Direction.WEST, gameMap.width / 2);
 
                             BestMoveTracker tracker = new BestMoveTracker();
 
@@ -237,7 +204,7 @@ public class StageOneBot {
                             checkAndUpdateBestMove(location, east, Direction.EAST, tracker);
                             checkAndUpdateBestMove(location, west, Direction.WEST, tracker);
 
-                            moves.add(new Move(location, Direction.NORTH));
+                            moves.add(new Move(location, tracker.bestMove.dir));
                             myLocations.remove(location);
                         }
                     }
