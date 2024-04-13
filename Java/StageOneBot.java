4bf591053e190f5ed98c6621015e833a07c38812
dandominicstaicu <dando.ds11@gmail.com>
2024-04-13 15:56:31 +0300

    failed bot - inner

--- Java/StageOneBot.java
+++ Java/StageOneBot.java
@@ -79,6 +79,55 @@ public class StageOneBot {
         }
     }
 
+    private static class BestMoveTracker {
+        public Move bestMove;
+        public double shortestDistance;
+
+        public BestMoveTracker() {
+            this.bestMove = null;
+            this.shortestDistance = Double.MAX_VALUE;
+        }
+    }
+
+    private static boolean isInnerLoc(int x, int y) {
+        Location location = gameMap.getLocation(x, y);
+        Site site = location.getSite();
+
+        Location north = gameMap.getLocation(location, Direction.NORTH);
+        Location south = gameMap.getLocation(location, Direction.SOUTH);
+        Location east = gameMap.getLocation(location, Direction.EAST);
+        Location west = gameMap.getLocation(location, Direction.WEST);
+
+
+        return north.getSite().owner == site.owner
+                && south.getSite().owner == site.owner
+                && west.getSite().owner == site.owner
+                && east.getSite().owner == site.owner;
+    }
+
+    private static void findFurthestOuter(Location far, Direction dir, Site site) {
+        int dist = 0;
+
+        while (far.getSite().owner == site.owner) {
+            if (dir.equals(Direction.NORTH) || dir.equals(Direction.SOUTH)) { // NORTH/SOUTH
+                if (++dist < gameMap.height / 2) {
+                    far = gameMap.getLocation(far, dir);
+                }
+            } else { // EAST/WEST
+                if (++dist < gameMap.width / 2) {
+                    far = gameMap.getLocation(far, dir);
+                }
+            }
+        }
+    }
+
+    private static void checkAndUpdateBestMove(Location location, Location loc, Direction dir, BestMoveTracker tracker) {
+        double dist = gameMap.getDistance(location, loc);
+        if (dist < tracker.shortestDistance) {
+            tracker.shortestDistance = dist;
+            tracker.bestMove = new Move(loc, dir);
+        }
+    }
 
     public static void main(String[] args) throws java.io.IOException {
 
@@ -128,17 +177,82 @@ public class StageOneBot {
 
             // dominic do your magic here. remove them from myLocations after assigning them work
 
+
+            for (int y = 0; y < gameMap.height; y++) {
+                for (int x = 0; x < gameMap.width; x++) {
+                    // move to closest border, functie
+                    // do not move to a site that already has 255
+                    // do not move if strenght < 5 * prod
+
+                    Location location = gameMap.getLocation(x, y);
+                    Site site = location.getSite();
+
+                    if (site.owner == myID) {
+                        if (!isInnerLoc(x, y)) {
+                            continue;
+                        }
+
+                        // do not move if strenght < 5 * prod
+                        // wait for it to increase
+                        if (site.strength < 5 * site.production) {
+                            Move newMove = new Move(location, Direction.STILL);
+                            moves.add(newMove);
+                            myLocations.remove(location);
+
+                        } else {
+                            Location north = gameMap.getLocation(location, Direction.NORTH);
+                            Location south = gameMap.getLocation(location, Direction.SOUTH);
+                            Location east = gameMap.getLocation(location, Direction.EAST);
+                            Location west = gameMap.getLocation(location, Direction.WEST);
+
+                            findFurthestOuter(north, Direction.NORTH, site);
+                            findFurthestOuter(south, Direction.SOUTH, site);
+                            findFurthestOuter(east, Direction.EAST, site);
+                            findFurthestOuter(west, Direction.WEST, site);
+
+
+//
+//                            PriorityQueue<Move> farMoves = new
+//                                    PriorityQueue<Move>(new Comparator<Move>() {
+//                                @Override
+//                                public int compare(Move o1, Move o2) {
+//                                    return (int) gameMap.getDistance(location, o1.loc) -
+//                                            (int) gameMap.getDistance(location, o2.loc);
+//                                }
+//                            });
+//
+//                            farMoves.add(new Move(north, Direction.NORTH));
+//                            farMoves.add(new Move(south, Direction.SOUTH));
+//                            farMoves.add(new Move(east, Direction.EAST));
+//                            farMoves.add(new Move(west, Direction.WEST));
+//
+//                            // Get the best move from the heap.
+//                            moves.add(new Move(location, farMoves.peek().dir));
+
+
+                            BestMoveTracker tracker = new BestMoveTracker();
+
+                            checkAndUpdateBestMove(location, north, Direction.NORTH, tracker);
+                            checkAndUpdateBestMove(location, south, Direction.SOUTH, tracker);
+                            checkAndUpdateBestMove(location, east, Direction.EAST, tracker);
+                            checkAndUpdateBestMove(location, west, Direction.WEST, tracker);
+
+                            moves.add(new Move(location, Direction.NORTH));
+                            myLocations.remove(location);
+                        }
+                    }
+                }
+            }
+
             while (!heap.isEmpty()) {
                 Location location = heap.poll();
                 conquer(location);
             }
 
-
             // for all weak exteriors
             for (Location location : myLocations) {
                 moves.add(new Move(location, Direction.STILL));
             }
-
 
             Networking.sendFrame(moves);
         }
